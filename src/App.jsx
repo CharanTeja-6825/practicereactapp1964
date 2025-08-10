9fc8b1b3512f1eabc3c3df64019ca4f840c27dde
CharanTeja <rcharanteja2006@gmail.com>
2025-08-10 10:47:02 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import viteLogo from '/vite.svg'
 function App() {
 
   return (
-    <div style={{color:"blue", display:"flex", justifyContent:"center", alignItems:"center", fontFamily:"monospace", height:"100vh", fontSize:"2.5rem"}}> 
+    <div style={{color:"blue", display:"flex", justifyContent:"center", alignItems:"center", fontFamily:"monospace", height:"100vh", fontSize:"2.5rem", flexDirection:"column"}}> 
         <h1>This is a practice <u>React app</u> for deployment on Github pages</h1>
+        <h2 style={{color:"#00ff00"}}>Deployment Status : SUCCESS</h2>
     </div>
   )
 }
