14f9b66cf9ee9743d4c7d9dbc5ea2c3763c66f71
CharanTeja <rcharanteja2006@gmail.com>
2025-08-10 10:10:01 +0530

    react app v1.0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <div style={{color:"blue", display:"flex", justifyContent:"center", alignItems:"center", fontFamily:"monospace", height:"100vh", fontSize:"2.5rem"}}> 
+        <h1>This is a practice <u>React app</u> for deployment on Github pages</h1>
+    </div>
+  )
+}
+
+export default App
